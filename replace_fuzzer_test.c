c580251d516cddb91b5dd6a7b65d1790d6c96147
Sebastian Bugge <vkbugge@hotmail.com>
2023-11-24 16:52:24 +0100

    Fix memory leak in fuzzer tester.

--- replace_fuzzer_test.c
+++ replace_fuzzer_test.c
@@ -5,12 +5,13 @@
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   // Create a c string from fuzzer data (with an additional byte set to '\0')
-  char *str = (char *)malloc(sizeof(char) * size + 1); // Create a c-string of length size + 1
-  memcpy(str, data, size);                             // Copy fuzzer data to string
-  str[size] = '\0';                                    // Set last byte of allocated string to '\0'
+  char *str = (char *)malloc(sizeof(char) * size +
+                             1); // Create a c-string of length size + 1
+  memcpy(str, data, size);       // Copy fuzzer data to string
+  str[size] = '\0';              // Set last byte of allocated string to '\0'
 
-  replace(str);
-
+  char *out = replace(str);
+  free(out);
   free(str);
 
   return 0;
